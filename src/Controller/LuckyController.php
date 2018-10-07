80213115e0b28a542bcda6cfbb3fada8d19b2712
sholomka <sholomka@gmail.com>
2018-10-07 18:12:40 +0300

    symfony

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number/{max}", name="app_lucky_number")
+     * @param $max
+     * @param Request $request
+     * @param SessionInterface $session
+     * @return Response
      */
-    public function number($max)
+    public function number($max, Request $request, SessionInterface $session)
     {
+
+
+        $this->addFlash('notice', 'fuck you and I am go away');
+
         $number = mt_rand(0, $max);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('base.html.twig');
     }
 }
